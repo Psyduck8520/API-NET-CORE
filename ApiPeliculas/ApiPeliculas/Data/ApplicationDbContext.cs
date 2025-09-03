6e0b7bb2163fa32f00a05c1d2ece7f95f371e900
Edison Quimbiamba <exprogresive@gmail.com>
2025-09-03 18:51:15 -0500

    Conexion con base de datos

---
+++ ApiPeliculas/ApiPeliculas/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ApiPeliculas.Data //necesitamos crear una carpeta Data y dentro de ella crear una clase llamada ApplicationDbContext que herede de DbContext
+{
+    public class ApplicationDbContext : DbContext //aki realizacion la importancion de Ent
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) // esto es un constructor que recibe las opciones de configuracion de la base de datos
+        {
+
+            
+        }
+        //Aki tenemos que pasar todos los modelos  o entidades que vamos a utilizar en la base de datos
+        public DbSet<Modelos.Categoria> Categoria { get; set; } //esto es una propiedad que representa una tabla en la base de datos
+
+
+    }
+}
---
+++ ApiPeliculas/ApiPeliculas/Modelos/Categoria.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiPeliculas.Modelos
+{
+    public class Categoria
+    {
+        [Key] // este atributo indica que esta propiedad es la clave primaria de la entidad
+        public int Id { get; set; }
+        [Required] // este atributo indica que esta propiedad es obligatoria
+        public string Nombre { get; set; }
+        [Required]
+        public DateTime FechaCreacion { get; set; }
+      
+
+
+
+    }
+}
